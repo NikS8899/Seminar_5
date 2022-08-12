342258f88cc392c856544a23d482eb613584cc06
NikitaS <sergio791309@gmail.com>
2022-08-12 22:28:23 +0300

    Create task 3 seminar 5

---
+++ Task 3/Task 3.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] GetArray (int size, int minRandom, int maxRandom)
+{
+    double [] array = new double [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRandom,maxRandom + 1);
+    }
+    return array;
+}
+double[] array= GetArray(10, 1, 999);
+
+
+double max = array[1];
+double min = array[1];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>max) max = array[i];
+    else if (array[i]<min) min = array[i];q
+}
+
+Console.WriteLine($"Результат: [ {String.Join("; ", array)} ] \nМаксимальное число: {max} \nМинимальное число: {min} \nРазницу между максимальным и минимальным элементов массива: {max - min}");
+
